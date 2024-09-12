5fba53f955c713b361473cbcdaef20be8428e8ef
ABDURAZZ <abdurazzokbossov@gmail.com>
2024-09-12 10:47:44 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,14 @@
+let uy_ishi = document.querySelector('body')
+uy_ishi.style.backgroundColor = 'red'
+
+uy_ishi.style.display = 'flex'
+uy_ishi.style.height = '100vh'
+uy_ishi.style.justifyContent = 'center'
+uy_ishi.style.alignItems = 'center'
+
+let h1 =  document.createElement('h1')
+h1.innerHTML = 
+
+
+prompt('ism krit va 100coin ber')
+ uy_ishi.appendChild(h1)
